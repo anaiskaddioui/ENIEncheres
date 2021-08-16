0240a8d1fc901ce96e3c7bde1b875e95a22da520
NoPainNoGainNoob <nopainnogainnoob@gmail.com>
2021-08-16 20:27:20 +0200

    top troisième commit

--- Projet/src/fr/eni/projet/Debut.java
+++ Projet/src/fr/eni/projet/Debut.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Debut
  */
-@WebServlet("/Debut")
+@WebServlet("urlpattern=/Debut")
 public class Debut extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        

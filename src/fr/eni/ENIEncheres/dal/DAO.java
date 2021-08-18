b2140c7c411e1c97a0676b31cbd3fa9055412557
BetaNu <ben.dubourguier@gmail.com>
2021-08-18 14:38:02 +0200

    Essai changement DAO et Utilisateurs.java

--- src/fr/eni/ENIEncheres/dal/DAO.java
+++ src/fr/eni/ENIEncheres/dal/DAO.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 	public interface DAO<T> { 
 
-		public void insert(T t) throws DALException;
-		public List<T> selectAll() throws DALException;
-		public T selectById(int id) throws DALException;
-		public List<T> selectByNom(String marque) throws DALException;
-		public List<T> selectByMotCle(String motCle) throws DALException;
-		public void update(T maj) throws DALException;
-		public void delete(int id) throws DALException;
+		public void insert(T t) throws Exception;
+		public List<T> selectAll() throws Exception;
+		public T selectById(int id) throws Exception;
+		public T selectByNom(String nom) throws Exception;
+		public List<T> selectByMotCle(String motCle) throws Exception;
+		public void update(T maj) throws Exception;
+		public void delete(int id) throws Exception;
 }

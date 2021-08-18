a7465a278cc50602a592c70244826d4e481831d6
jl <jl@asri>
2021-08-18 22:39:09 +0200

    Objet Article Vendu couche dal

---
+++ src/fr/eni/ENIEncheres/dal/DAOArticleVendu.java
@@ -0,0 +1,18 @@
+package fr.eni.ENIEncheres.dal;
+
+import java.util.List;
+
+import fr.eni.ENIEncheres.bo.Categorie;
+import fr.eni.ENIEncheres.bo.Utilisateurs;
+
+public interface DAOArticleVendu<ArticleVendu> {
+	
+	
+	    List<ArticleVendu> filterByCategory(Categorie categorie) throws DALException;
+	    List<ArticleVendu> filterByString(String filter) throws DALException;
+	    List<Integer> filterByEtat(String etat) throws DALException;
+	    List<Integer> getArticlesFromASellerAndState (Utilisateurs utilisateur, String state) throws DALException;
+	    void updateCurrentPrice(int noArticle, int newPrice) throws DALException;
+	}
+
+

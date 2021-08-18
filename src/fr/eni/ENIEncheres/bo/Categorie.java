19b69f8c831724ccd561055a7c037dd226d0aab9
jl <jl@asri>
2021-08-18 22:12:10 +0200

    je commite sur ma base

---
+++ src/fr/eni/ENIEncheres/bo/Categorie.java
@@ -0,0 +1,46 @@
+package fr.eni.ENIEncheres.bo;
+
+import java.util.ArrayList;
+
+public class Categorie {
+
+	private int idCategorie;
+	private String libelle;
+	private ArrayList<ArticleVendu> categorieArticle = null;
+
+	public Categorie() {
+		super();
+	}
+
+	public int getIdCategorie() {
+		return idCategorie;
+	}
+
+	public void setIdCategorie(int idCategorie) {
+		this.idCategorie = idCategorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public ArrayList<ArticleVendu> getCategorieArticle() {
+		return categorieArticle;
+	}
+
+	public void setCategorieArticle(ArrayList<ArticleVendu> categorieArticle) {
+		this.categorieArticle = categorieArticle;
+	}
+
+	@Override
+	public String toString() {
+		return "Categorie [idCategorie=" + idCategorie + ", libelle=" + libelle + ", categorieArticle="
+				+ categorieArticle + ", getIdCategorie()=" + getIdCategorie() + ", getLibelle()=" + getLibelle()
+				+ ", getCategorieArticle()=" + getCategorieArticle() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	}
+}

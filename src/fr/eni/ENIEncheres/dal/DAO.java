15ce2f706d78243a1b984528e5d568535a6dda57
anaiskaddioui <an.17@hotmail.fr>
2021-08-18 10:45:14 +0200

    Ajout DAO générique (.dal)

---
+++ src/fr/eni/ENIEncheres/dal/DAO.java
@@ -0,0 +1,15 @@
+package fr.eni.ENIEncheres.dal;
+
+import java.util.List;
+
+
+	public interface DAO<T> { 
+
+		public void insert(T t) throws DALException;
+		public List<T> selectAll() throws DALException;
+		public T selectById(int id) throws DALException;
+		public List<T> selectByNom(String marque) throws DALException;
+		public List<T> selectByMotCle(String motCle) throws DALException;
+		public void update(T maj) throws DALException;
+		public void delete(int id) throws DALException;
+}
